7644378ca62b47a682d1d5bb9fd65ac16529b020
Yurii Kulagin <yuk.178@bk.ru>
2022-11-11 22:09:16 +0300

    Initial commit

---
+++ src/test/java/TestV1.java
@@ -0,0 +1,29 @@
+
+
+import org.junit.jupiter.api.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class TestV1 {
+    @Test
+    void Return() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("-наша проверка здесь-") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("-наша проверка здесь-"))
+
+        ;
+    }
+}
